b7f4ceaf17487ee006f1b10f9845ce181abac54f
daneshwaran <daneshwaranm@gmail.com>
2021-11-01 17:52:13 +0530

    UI Nav bar added
      *Created three pages
      *Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,50 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import List from './components/List';
-import AddToList from './components/AddToList';
+import { Switch, Route } from 'react-router-dom';
+import Products from './pages/products';
+import Cart from './pages/cart';
+import Order from './pages/orders';
+import MainNavigation from './components/layout/MainNavigation';
 
 export interface IState {
   people: {
-      name: string
-      age: number
-      img: string
-      note?: string
-  }[]
+    name: string;
+    age: number;
+    img: string;
+    note?: string;
+  }[];
 }
 
-
 function App() {
-
-  const [people, setPeople] = useState<IState["people"]>([
+  const [people, setPeople] = useState<IState['people']>([
     {
-      name: "LeBron James",
+      name: 'LeBron James111',
       age: 35,
-      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
-      note: "Allegeric to staying on the same team",
+      img: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      note: 'Allegeric to staying on the same team',
     },
     {
-      name: "Kobe Bryant",
+      name: 'Kobe Bryant',
       age: 42,
-      img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg"
-    }
-  ])
+      img:
+        'https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg',
+    },
+  ]);
 
   return (
-    <div className="App">
-      <h1>People Invited to my Party</h1>
-      <List people={people}/>
-      <AddToList setPeople={setPeople} people={people}/>
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <Products />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/orders'>
+          <Order />
+        </Route>
+      </Switch>
     </div>
   );
 }
